a5581b966e36850a042241e3cb4eb31cdb9c5d09
HYDR00GEN <ovy.hydrogen@gmail.com>
2022-11-16 17:39:16 +0100

    es3

--- query.sql
+++ query.sql
@@ -9,3 +9,9 @@ where citta = 'Pisa'
 select *
 from film
 where film.regista like '%Fellini%' and annoproduzione > 1960
+
+/** 3 - titolo durata(non c'è) film fantascienza giapp o fr dopo il 1990**/
+
+select titolo, genere, nazionalita, annoproduzione
+from film
+where genere like '%antascienza%' and nazionalita = 'Giapponese' or nazionalita = 'Francese' and annoproduzione > 1990
